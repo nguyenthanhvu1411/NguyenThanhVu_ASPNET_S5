77e9d32b56619efce6c6333cf0dbac14c01813ca
ThanhVU <nthanhvu1411@gmail.com>
2026-03-12 08:09:51 +0700

    new project

--- NguyenThanhVu_2123110350/Controllers/WeatherForecastController.cs
+++ NguyenThanhVu_2123110350/Controllers/WeatherForecastController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Demo01.Controllers
+namespace NguyenThanhVu_2123110350.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries =
-        [
+        private static readonly string[] Summaries = new[]
+        {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
@@ -22,6 +29,5 @@ namespace Demo01.Controllers
             })
             .ToArray();
         }
-        
     }
 }
